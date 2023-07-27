bfae74c71fc76a31f33287e1e711906de1a5adcf
Shailesh Gehlot <shailesh.gehlot.sg@gmail.com>
2023-07-28 01:40:52 +0530

    Update crisp-chat.tsx

--- components/crisp-chat.tsx
+++ components/crisp-chat.tsx
@@ -5,7 +5,7 @@ import { Crisp } from "crisp-sdk-web";
 
 export const CrispChat = () => {
   useEffect(() => {
-    Crisp.configure("8254f25a-eca0-421e-b4de-2bf655c03887");
+    Crisp.configure("1fbca9bc-05ab-430b-8a8d-2bf2ac6c2c88");
   }, []);
 
   return null;
